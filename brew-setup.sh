697076d92dcc087b76fee628b91c000156ea51a4
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-21 20:51:48 +0000

    added Linux links

--- brew-setup.sh
+++ brew-setup.sh
@@ -1,20 +1,56 @@
 
 echo | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew analytics off
+
+# Linux: https://releases.hashicorp.com/consul/0.8.3/consul_0.8.3_linux_amd64.zip
 brew install consul
+
+# Linux: https://releases.hashicorp.com/nomad/0.5.6/nomad_0.5.6_linux_amd64.zip
 brew install nomad
+
+# Linux:  Pre-installed
 brew install gawk
+
+# apt-get jq
 brew install jq
+
+# For linux: curl -L https://git.io/n-install | bash
 brew install n
+
 n lts
+
+# Linux: https://dl.minio.io/server/minio/release/linux-amd64/minio
 brew install minio/stable/minio
+
+# Linux: https://dl.minio.io/client/mc/release/linux-amd64/mc
 brew install minio/stable/mc
+
+# Linux: 
+# wget https://dl.influxdata.com/influxdb/releases/influxdb_1.2.4_amd64.deb
+# sudo dpkg -i influxdb_1.2.4_amd64.deb
 brew install influxdb
+
+# Linux: 
+# wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_4.2.0_amd64.deb
+# sudo dpkg -i grafana_4.2.0_amd64.deb
 brew install grafana
+
+# Linux: 
+# wget https://dl.influxdata.com/telegraf/releases/telegraf_1.3.0-1_amd64.deb
+# sudo dpkg -i telegraf_1.3.0-1_amd64.deb
 brew install telegraf
+
+# Linux: https://releases.hashicorp.com/terraform/0.9.5/terraform_0.9.5_linux_amd64.zip
 brew install terraform
+
+# Linux: https://releases.hashicorp.com/vault/0.7.2/vault_0.7.2_linux_amd64.zip
 brew install vault
+
+# Linux: https://caddyserver.com/download/linux/amd64
+# Or: curl https://getcaddy.com | bash
 brew install caddy
+
+# I have no idea how to install R on Linux, but not necessary?
 brew tap homebrew/science
 brew install R
 
@@ -35,14 +71,17 @@ curl -L -O 'https://github.com/containous/traefik/releases/download/v1.2.3/traef
 cp traefik_darwin-amd64 /usr/local/bin/traefik
 chmod 755 /usr/local/bin/traefik
 
+# Linux: a linux build will be in bin/linux
 # This is a custom build of restic
 cp bin/restic /usr/local/bin
 
+# Not needed for Linux...
 # This is a custom build of influxdb-relay
 cp bin/influxdb-relay /usr/local/bin
 
 # Install packages from github masters:
 
+# Needed for linux?
 git clone https://github.com/ctberthiaume/time-series-cop
 cd time-series-cop
 npm install -g
