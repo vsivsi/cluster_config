309c2a229ff81c879a94a746d0046ecafee73286
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-21 22:30:59 +0000

    added cluster_services script

---
+++ cluster-services.sh
@@ -0,0 +1,32 @@
+mkdir -p /usr/local/var/consul
+mkdir -p /usr/local/etc/consul.d
+
+mkdir -p /usr/local/var/nomad
+mkdir -p /usr/local/etc/nomad.d
+
+mkdir -p /usr/local/etc/jobs
+
+case $1 in
+(mm0)
+  NODETYPE="head";;
+(mm?)
+  NODETYPE="worker";;
+(*)
+  NODETYPE="dev";;
+esac
+
+echo "Setting up $NODETYPE node"
+
+cp config/consul_$NODETYPE.json /usr/local/etc/consul.d
+cp config/nomad_$NODETYPE.hcl /usr/local/etc/nomad.d
+cp jobs/* /usr/local/etc/jobs/
+
+sudo cp consul.plist /Library/LaunchDaemons/com.hashicorp.consul.plist
+sudo launchctl unload /Library/LaunchDaemons/com.hashicorp.consul.plist
+sudo launchctl load /Library/LaunchDaemons/com.hashicorp.consul.plist
+sudo cp nomad.plist /Library/LaunchDaemons/com.hashicorp.nomad.plist
+sudo launchctl unload /Library/LaunchDaemons/com.hashicorp.nomad.plist
+sudo launchctl load /Library/LaunchDaemons/com.hashicorp.nomad.plist
+sudo mkdir -p /etc/resolver
+sudo cp consul.resolver /etc/resolver/consul
+
