cf3fa6cd404ef69ede4f8227450ecefba1478ecd
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-24 21:29:48 +0000

    Initial terraform config with some consul key definitions and a nomad job to start the hashi-ui service on the head node

---
+++ jobs/clust.tf
@@ -0,0 +1,71 @@
+terraform {
+  backend "consul" {
+    address = "mm0:8500"
+    path    = "terraform"
+  }
+}
+
+provider "consul" {
+  address    = "mm0:8500"
+  datacenter = "dc1"
+}
+
+resource "consul_key_prefix" "server_setup" {
+
+   path_prefix = "server/"
+   subkeys     = {
+      DNS_NAME = "dev.local"
+      MINIO_PREFIX = "/minio2"
+      MINIO_HEAD_PREFIX = "/minio"
+   }
+}
+
+resource "consul_key_prefix" "server_setup" {
+
+   path_prefix = "server/"
+   subkeys     = {
+      DNS_NAME = "dev.local"
+      MINIO_PREFIX = "/minio2"
+      MINIO_HEAD_PREFIX = "/minio"
+   }
+}
+
+resource "consul_key_prefix" "minio_config" {
+  path_prefix = "minio/"
+
+  subkeys = {
+
+    MINIO_ACCESS_KEY = "${var.MINIO_ACCESS_KEY}",
+    MINIO_SECRET_KEY = "${var.MINIO_SECRET_KEY}"
+  }
+}
+
+resource "consul_key_prefix" "minio_head_config" {
+  path_prefix = "minio-head/"
+
+  subkeys = {
+
+    MINIO_ACCESS_KEY = "${var.MINIO_HEAD_ACCESS_KEY}",
+    MINIO_SECRET_KEY = "${var.MINIO_HEAD_SECRET_KEY}"
+  }
+}
+
+provider "nomad" {
+  address = "http://mm0:4646"
+}
+
+resource "nomad_job" "hashi-ui" {
+   jobspec = "${file("hashi-ui.nomad")}"
+}
+
+# resource "nomad_job" "minio-head" {
+#    jobspec = "${file("minio-head.nomad")}"
+# }
+
+# resource "nomad_job" "minio" {
+#    jobspec = "${file("minio.nomad")}"
+# }
+
+# resource "nomad_job" "caddy" {
+#    jobspec = "${file("caddy.nomad")}"
+# }
---
+++ jobs/hashi-ui.nomad
@@ -0,0 +1,49 @@
+job "hashi-ui" {
+  datacenters = ["dc1"]
+  type = "service"
+
+  constraint {
+    attribute = "${node.class}"
+    value     = "head"
+  }
+
+  group "hashi-ui" {
+    task "hashi-ui" {
+      driver = "raw_exec"
+      config {
+        command = "/usr/local/bin/hashi-ui"
+        args = ["--nomad-enable", "--consul-enable", "--listen-address=0.0.0.0:3001"]
+#        args = ["--nomad-enable", "--consul-enable", "--listen-address=0.0.0.0:3001", "--proxy-address", "acgt.ocean.washington.edu/hashi-ui"]
+      }
+
+      resources {
+        cpu = 20
+        memory = 10
+        network {
+          mbits = 10
+          port "hashi_ui" {
+             static = 3001
+          }
+        }
+      }
+
+      service {
+        name = "hashi-ui"
+        tags = ["monitoring"]
+        port = "hashi_ui" 
+        check {
+          name     = "Hashi-ui tcp check"
+          type     = "tcp"
+          interval = "15s"
+          timeout  = "5s"
+        }
+      }
+
+      logs {
+        max_files = 3
+        max_file_size = 10
+      }
+    }
+  }
+}
+
