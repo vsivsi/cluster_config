005c841d0a5a80fb32844f14e178ab370d601133
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-25 17:14:43 +0000

    Correct parameter ordering

--- bin/snapshot_hook.sh
+++ bin/snapshot_hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/hook.log
-nomad job dispatch import_snapshot -meta SNAPSHOT_PATH=$1
+nomad job dispatch -meta SNAPSHOT_PATH=$1 import_snapshot 
