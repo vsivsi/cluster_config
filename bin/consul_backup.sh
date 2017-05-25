0c7cad61e99f6dc743a9d58430fedcc55332e40e
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-25 07:09:02 +0000

    Ensure that every backup has the same hostname for aging.

--- bin/consul_backup.sh
+++ bin/consul_backup.sh
@@ -2,4 +2,4 @@
 FN=backup.snap
 /usr/local/bin/restic init
 /usr/local/bin/consul snapshot save $FN
-cat $FN | /usr/local/bin/restic backup --stdin --stdin-filename $FN
+cat $FN | /usr/local/bin/restic backup --stdin --stdin-filename $FN --hostname consul
