d2da39d9e67d50bb373cfb79fec4aa1570c22e69
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-25 17:47:10 +0000

    Full path to execs! Always!

--- bin/snapshot_hook.sh
+++ bin/snapshot_hook.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/hook.log
-nomad job dispatch -meta SNAPSHOT_PATH=$1 import_snapshot 
+/usr/local/bin/nomad job dispatch -meta SNAPSHOT_PATH=$1 import_snapshot 
