6a0930c4f7254c1e1bf5ed4145a4e0a08f4dea58
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-25 06:14:25 +0000

    Use consul snapshot instead of consul-backinator

--- bin/consul_backup.sh
+++ bin/consul_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+FN=backup.snap
 /usr/local/bin/restic init
-/usr/local/bin/consul-backinator backup
-/usr/local/bin/restic backup ./consul.bak*
+/usr/local/bin/consul snapshot save $FN
+cat $FN | /usr/local/bin/restic backup --stdin --stdin-filename $FN
