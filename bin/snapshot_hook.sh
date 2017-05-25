a18cf106b0a4f61b4dd780a0ba80ea1251adaf6f
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-25 08:56:30 +0000

    Write log to common location

--- bin/snapshot_hook.sh
+++ bin/snapshot_hook.sh
@@ -1,2 +1,2 @@
-echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $2 >> hook.log
-echo $1 | jq '.' > output.json
+echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $2 >> /usr/local/var/hook.log
+echo $1 | jq '.' > /usr/local/var/output.json
