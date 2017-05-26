b03e64d88777960b041eaa1e862112946c7e7868
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-26 02:21:14 +0000

    Fix missing pipe

--- bin/grafana_backup.sh
+++ bin/grafana_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 FN=grafana.bak
 /usr/local/bin/restic init
-echo ".backup /tmp/$FN" /usr/bin/sqlite3 /usr/local/var/lib/grafana/grafana.db
+echo ".backup /tmp/$FN" | /usr/bin/sqlite3 /usr/local/var/lib/grafana/grafana.db
 cat /tmp/$FN | /usr/local/bin/restic backup --stdin --stdin-filename $FN --hostname grafana
 rm /tmp/$FN
