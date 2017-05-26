dac11829c17d80041dff72be48577c7502787271
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-26 09:15:27 +0000

    Log the env

--- bin/process_upload.sh
+++ bin/process_upload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Got upload for $2 on $1" >> /usr/local/var/log/uploads.txt
+/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/bin/env > /usr/local/var/log/upload_env.txt
 /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic init -r ${RESTIC_REPO_ROOT}${2}
 cat $1 | /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic backup -r ${RESTIC_REPO_ROOT}${2} --stdin --stdin-filename $2.tsv --hostname uploader
