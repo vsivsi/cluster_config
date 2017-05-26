907b4466dd7318910175a3369242f31af0709b95
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-26 01:22:19 +0000

    Fix broken backup on non-head nodes. Also use a consistent path so restic forget will work (and so data won’t pile up in nomad instances)

--- bin/influx_backup.sh
+++ bin/influx_backup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 /usr/local/bin/restic init
-/usr/local/bin/influxd backup -database data data
-/usr/local/bin/restic backup --hostname influx ./data
-/usr/local/bin/influxd backup -database viz viz
-/usr/local/bin/restic backup --hostname influx ./viz
+/usr/local/bin/influxd backup -host influx-head.service.consul:8088 -database data /tmp/data
+/usr/local/bin/restic backup --hostname influx /tmp/data
+/usr/local/bin/influxd backup -host influx-head.service.consul:8088 -database viz /tmp/viz
+/usr/local/bin/restic backup --hostname influx /tmp/viz
+rm -r /tmp/{data,viz}
