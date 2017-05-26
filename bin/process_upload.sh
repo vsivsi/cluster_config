f101f1b9fef647249825906162b971890c683750
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-26 09:10:55 +0000

    Separate env vars

--- bin/process_upload.sh
+++ bin/process_upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Got upload for $2 on $1" >> /usr/local/var/log/uploads.txt
-/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic init -r $RESTIC_REPO_ROOT$2
-cat $1 | /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic backup -r $RESTIC_REPO_ROOT$2 --stdin --stdin-filename $2.tsv --hostname uploader
+/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic init -r ${RESTIC_REPO_ROOT}${2}
+cat $1 | /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic backup -r ${RESTIC_REPO_ROOT}${2} --stdin --stdin-filename $2.tsv --hostname uploader
