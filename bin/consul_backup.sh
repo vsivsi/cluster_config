e2639a0ee256d74b23c941a3a64191210c440b46
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-26 02:50:48 +0000

    Apparently proc subst doesn’t work under nomad.

--- bin/consul_backup.sh
+++ bin/consul_backup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 FN=backup.snap
 /usr/local/bin/restic init
-/usr/local/bin/consul snapshot save >(/usr/local/bin/restic backup --stdin --stdin-filename $FN --hostname consul)
+/usr/local/bin/consul snapshot save /tmp/$FN
+cat /tmp/$FN | /usr/local/bin/restic backup --stdin --stdin-filename $FN --hostname consul
+rm /tmp/$FN
