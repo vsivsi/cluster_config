4b7cf0dc0f1e2c71d6dd1b27b5d20d4ae2b07d2a
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-26 09:22:44 +0000

    Try just the root

--- bin/process_upload.sh
+++ bin/process_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Got upload for $2 on $1" >> /usr/local/var/log/uploads.txt
-/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /bin/echo ${RESTIC_REPO_ROOT}${2} > /usr/local/var/log/upload_env.txt
+/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /bin/echo ${RESTIC_REPO_ROOT} > /usr/local/var/log/upload_env.txt
 /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic init -r ${RESTIC_REPO_ROOT}${2}
 cat $1 | /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic backup -r ${RESTIC_REPO_ROOT}${2} --stdin --stdin-filename $2.tsv --hostname uploader
