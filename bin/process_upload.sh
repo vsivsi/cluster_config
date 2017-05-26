6b8aabd3187993e9c51a87a9f408688cab80ffc6
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-26 15:44:58 +0000

    Dump all the things

--- bin/process_upload.sh
+++ bin/process_upload.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Got upload for $2 on $1" >> /usr/local/var/log/uploads.txt
-/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /bin/echo ${RESTIC_REPO_ROOT} > /usr/local/var/log/upload_env.txt
+/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/bin/env > /usr/local/var/log/upload_env.txt
+/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /bin/echo "Root:" ${RESTIC_REPO_ROOT} > /usr/local/var/log/upload_restic.txt
+/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /bin/echo "Name:" ${2} >> /usr/local/var/log/upload_restic.txt
+/usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /bin/echo "RootName:" ${RESTIC_REPO_ROOT}${2} >> /usr/local/var/log/upload_restic.txt
 /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic init -r ${RESTIC_REPO_ROOT}${2}
 cat $1 | /usr/local/bin/envconsul -prefix=minio/ -prefix=uploadbak/ /usr/local/bin/restic backup -r ${RESTIC_REPO_ROOT}${2} --stdin --stdin-filename $2.tsv --hostname uploader
