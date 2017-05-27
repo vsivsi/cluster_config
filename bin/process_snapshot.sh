eb8db4326f472b7dddd1d73b1c6cd6b44fdb5dac
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 03:35:33 +0000

    Use simpler variables

--- bin/process_snapshot.sh
+++ bin/process_snapshot.sh
@@ -5,18 +5,18 @@ SNAPID=${1#*/snapshots/}
 DATASET=${1%/snapshots/*}
 
 echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/log/hook_processed.log
-echo "Restic repo :" $(consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY) >> /usr/local/var/log/hook_processed.log
-echo "Snapshot ID: " ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
-echo "Script to run: $(consul kv get ${1%/snapshots/*}/script)" >> /usr/local/var/log/hook_processed.log
+echo "Restic repo :" $(consul kv get $DATASET/RESTIC_REPOSITORY) >> /usr/local/var/log/hook_processed.log
+echo "Snapshot ID: " $SNAPID >> /usr/local/var/log/hook_processed.log
+echo "Script to run: $(consul kv get $DATASET/script)" >> /usr/local/var/log/hook_processed.log
 
 echo "DATASET: $DATASET  SNAPID: $SNAPID" >> /usr/local/var/log/hook_processed.log
 
-envconsul -prefix minio/ -prefix ${1%/snapshots/*}/ restic restore -t /tmp/${1#*/snapshots/} ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
+envconsul -prefix minio/ -prefix $DATASET/ restic restore -t /tmp/$SNAPID $SNAPID >> /usr/local/var/log/hook_processed.log
 
-ls /tmp/${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
+ls /tmp/$DATASET >> /usr/local/var/log/hook_processed.log
 
-$(consul kv get ${1%/snapshots/*}/script) -i /tmp/${1#*/snapshots/}/*.tsv -H influx-head.service.consul -d data
+$(consul kv get $DATASET/script) -i /tmp/$SNAPID/*.tsv -H influx-head.service.consul -d data
 
-$(consul kv get ${1%/snapshots/*}/script) -i /tmp/${1#*/snapshots/}/*.tsv -o /dev/stdout >> /usr/local/var/log/hook_processed.log
+$(consul kv get $DATASET/script) -i /tmp/$SNAPID/*.tsv -o /dev/stdout >> /usr/local/var/log/hook_processed.log
 
-rm -r /tmp/${1#*/snapshots/}
+rm -r /tmp/$SNAPID
