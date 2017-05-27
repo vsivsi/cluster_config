87da66cc6c186fc97af7468704ff94dab85a8165
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 01:04:15 +0000

    Actually write output

--- bin/process_snapshot.sh
+++ bin/process_snapshot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/log/hook_processed.log
-echo "Restic repo :" $(/usr/local/bin/consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY)
-echo "Snapshot ID: " ${1#*/snapshots/}
-echo "Script to run: /usr/local/bin/$(/usr/local/bin/consul kv get ${1%/snapshots/*}/script)"
+echo "Restic repo :" $(/usr/local/bin/consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY) >> /usr/local/var/log/hook_processed.log
+echo "Snapshot ID: " ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
+echo "Script to run: /usr/local/bin/$(/usr/local/bin/consul kv get ${1%/snapshots/*}/script)" >> /usr/local/var/log/hook_processed.log
