e1b81fc8bf7579cb2c193b0a415e05fd7025a06e
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 05:37:48 +0000

    Setup to use with envconsul

--- bin/fetch_ups_data.sh
+++ bin/fetch_ups_data.sh
@@ -6,21 +6,21 @@
 export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 # Default read only user/pass
-export CPWUSER=device
-export CPWPASS=cyber
-# Default IP address
-export UPSIP=192.168.105.38
+# export CPWUSER=device
+# export CPWPASS=cyber
+# # Default IP address
+# export UPSIP=192.168.105.38
 
 # All HTTP headers are dumped, omit "-D -" to suppress
-curl -D - "http://$UPSIP/login_pass.cgi?username=$CPWUSER&password=$CPWPASS&action=LOGIN"
-curl -D - -c cookiejar.txt "http://$UPSIP/login_pass.html"
-curl -b cookiejar.txt -D - "http://$UPSIP/login_counter.html?stap=1"
-curl -b cookiejar.txt -D - "http://$UPSIP/login_counter.html?stap=2"
-sleep 1  # This seems to occasionally help
-curl -b cookiejar.txt -D - "http://$UPSIP/login_counter.html?stap=2"
-curl -b cookiejar.txt -D - "http://$UPSIP/login.cgi?action=LOGIN"
+curl "http://$UPSIP/login_pass.cgi?username=$CPWUSER&password=$CPWPASS&action=LOGIN" 1>&2
+curl -c cookiejar.txt "http://$UPSIP/login_pass.html" 1>&2
+curl -b cookiejar.txt "http://$UPSIP/login_counter.html?stap=1" 1>&2
+curl -b cookiejar.txt "http://$UPSIP/login_counter.html?stap=2" 1>&2
+sleep 2  # This seems to occasionally help
+curl -b cookiejar.txt "http://$UPSIP/login_counter.html?stap=2" 1>&2
+curl -b cookiejar.txt "http://$UPSIP/login.cgi?action=LOGIN" 1>&2
 # HTTP headers are written to the top of these files, omit "-D -" to suppress
-curl -b cookiejar.txt -D - "http://$UPSIP/get_eventLog.cgi" > eventLog.txt
-curl -b cookiejar.txt -D - "http://$UPSIP/get_dataRec.cgi" > dataLog.txt
+# curl -b cookiejar.txt "http://$UPSIP/get_eventLog.cgi" > eventLog.txt
+curl -b cookiejar.txt "http://$UPSIP/get_dataRec.cgi" > dataLog.txt
 # This is necessary or future auth attempts will be locked out until some timeout
-curl -b cookiejar.txt -D - "http://$UPSIP/logout.html"
+curl -b cookiejar.txt "http://$UPSIP/logout.html" 1>&2
