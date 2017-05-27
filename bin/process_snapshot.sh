2ee35a2cdb22cd5c1b3cf73d129cf28204c0dc7e
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 02:45:26 +0000

    Try out simplifying things a bit

--- bin/process_snapshot.sh
+++ bin/process_snapshot.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
+SNAPID=${1#*/snapshots/}
+DATASET=${1%/snapshots/*}
+
 echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/log/hook_processed.log
 echo "Restic repo :" $(consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY) >> /usr/local/var/log/hook_processed.log
 echo "Snapshot ID: " ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
 echo "Script to run: $(consul kv get ${1%/snapshots/*}/script)" >> /usr/local/var/log/hook_processed.log
+
+echo "DATASET: $DATASET  SNAPID: $SNAPID" >> /usr/local/var/log/hook_processed.log
 
 envconsul -prefix minio/ -prefix ${1%/snapshots/*}/ restic restore -t /tmp/${1#*/snapshots/} ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
 
