d758879fe33e21c249c088c0b302cb33b11635f0
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 02:33:29 +0000

    Get smart

--- bin/process_snapshot.sh
+++ bin/process_snapshot.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/log/hook_processed.log
-echo "Restic repo :" $(/usr/local/bin/consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY) >> /usr/local/var/log/hook_processed.log
+echo "Restic repo :" $(consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY) >> /usr/local/var/log/hook_processed.log
 echo "Snapshot ID: " ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
-echo "Script to run: /usr/local/bin/$(/usr/local/bin/consul kv get ${1%/snapshots/*}/script)" >> /usr/local/var/log/hook_processed.log
+echo "Script to run: $(consul kv get ${1%/snapshots/*}/script)" >> /usr/local/var/log/hook_processed.log
 
-/usr/local/bin/envconsul -prefix minio/ -prefix ${1%/snapshots/*}/ /usr/local/bin/restic restore -t /tmp/${1#*/snapshots/} ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
+envconsul -prefix minio/ -prefix ${1%/snapshots/*}/ restic restore -t /tmp/${1#*/snapshots/} ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
 
 ls /tmp/${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
 
-/usr/local/bin/$(/usr/local/bin/consul kv get ${1%/snapshots/*}/script) -i /tmp/${1#*/snapshots/}/*.tsv -H influx-head.service.consul -d data
+$(consul kv get ${1%/snapshots/*}/script) -i /tmp/${1#*/snapshots/}/*.tsv -H influx-head.service.consul -d data
 
-/usr/local/bin/node /usr/local/bin/$(/usr/local/bin/consul kv get ${1%/snapshots/*}/script) -i /tmp/${1#*/snapshots/}/*.tsv -o /dev/stdout >> /usr/local/var/log/hook_processed.log
+$(consul kv get ${1%/snapshots/*}/script) -i /tmp/${1#*/snapshots/}/*.tsv -o /dev/stdout >> /usr/local/var/log/hook_processed.log
