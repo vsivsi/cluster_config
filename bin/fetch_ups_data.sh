c57ceeeed4b1e48089685f55c30554864808a6df
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 05:53:04 +0000

    Wire it up to restic

--- bin/fetch_ups_data.sh
+++ bin/fetch_ups_data.sh
@@ -11,6 +11,8 @@ export PATH=$PATH:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 # # Default IP address
 # export UPSIP=192.168.105.38
 
+restic init
+
 # All HTTP headers are dumped, omit "-D -" to suppress
 curl "http://$UPSIP/login_pass.cgi?username=$CPWUSER&password=$CPWPASS&action=LOGIN" 1>&2
 curl -c cookiejar.txt "http://$UPSIP/login_pass.html" 1>&2
@@ -21,6 +23,6 @@ curl -b cookiejar.txt "http://$UPSIP/login_counter.html?stap=2" 1>&2
 curl -b cookiejar.txt "http://$UPSIP/login.cgi?action=LOGIN" 1>&2
 # HTTP headers are written to the top of these files, omit "-D -" to suppress
 # curl -b cookiejar.txt "http://$UPSIP/get_eventLog.cgi" > eventLog.txt
-curl -b cookiejar.txt "http://$UPSIP/get_dataRec.cgi" > dataLog.txt
+curl -b cookiejar.txt "http://$UPSIP/get_dataRec.cgi" | restic backup --stdin --stdin-filename ups-data.txt --hostname cyberpower
 # This is necessary or future auth attempts will be locked out until some timeout
 curl -b cookiejar.txt "http://$UPSIP/logout.html" 1>&2
