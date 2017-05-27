edcd1e843dd6d046d35365ed78d5960e8d9666cd
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 00:57:07 +0000

    Setup bash parsing of the snapshot path elements and consul lookups required to process

--- bin/process_snapshot.sh
+++ bin/process_snapshot.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/log/hook_processed.log
+echo "Restic repo :" $(/usr/local/bin/consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY)
+echo "Snapshot ID: " ${1#*/snapshots/}
+echo "Script to run: /usr/local/bin/$(/usr/local/bin/consul kv get ${1%/snapshots/*}/script)"
