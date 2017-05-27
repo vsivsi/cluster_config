c5bf2036e19c426cf37bb5889ac1a113f6dc4716
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 01:17:09 +0000

    Really fix it this time

--- bin/process_upload.sh
+++ bin/process_upload.sh
@@ -2,4 +2,4 @@
 echo "Backing up upload for $2 at $1" # >> /usr/local/var/log/uploads.txt
 
 envconsul -prefix=minio/ -prefix=uploadbackups/$2/ restic init
-cat $1 | envconsul -prefix=minio/ -prefix=uploadbackups/$2/ restic backup --stdin --stdin-filename $1.tsv --hostname uploader
+cat $1 | envconsul -prefix=minio/ -prefix=uploadbackups/$2/ restic backup --stdin --stdin-filename $2.tsv --hostname uploader
