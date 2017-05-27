c4a43a6009ccc6dc14558a84d563fb9190e5f3ec
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 03:22:28 +0000

    Confirm cruise ID arg

--- bin/process_upload.sh
+++ bin/process_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Backing up upload for $2 at $1" # >> /usr/local/var/log/uploads.txt
+echo "Backing up upload for $2, cruise $3 at $1" # >> /usr/local/var/log/uploads.txt
 
 envconsul -prefix=minio/ -prefix=uploadbackups/$2/ restic init
 cat $1 | envconsul -prefix=minio/ -prefix=uploadbackups/$2/ restic backup --stdin --stdin-filename $2.tsv --hostname uploader
