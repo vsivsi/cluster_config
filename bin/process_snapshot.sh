539434ef33ae16693347bfe69391e469bc7d0daa
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-27 02:08:09 +0000

    Add restic restore to tmp directory

--- bin/process_snapshot.sh
+++ bin/process_snapshot.sh
@@ -3,3 +3,7 @@ echo `date -u +"%Y-%m-%dT%H:%M:%SZ"` $1 >> /usr/local/var/log/hook_processed.log
 echo "Restic repo :" $(/usr/local/bin/consul kv get ${1%/snapshots/*}/RESTIC_REPOSITORY) >> /usr/local/var/log/hook_processed.log
 echo "Snapshot ID: " ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
 echo "Script to run: /usr/local/bin/$(/usr/local/bin/consul kv get ${1%/snapshots/*}/script)" >> /usr/local/var/log/hook_processed.log
+
+/usr/local/bin/envconsul -prefix minio/ -prefix ${1%/snapshots/*}/ /usr/local/bin/restic restore -t /tmp/${1#*/snapshots/} ${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
+
+ls /tmp/${1#*/snapshots/} >> /usr/local/var/log/hook_processed.log
