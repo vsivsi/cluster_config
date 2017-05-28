547c25586635ce52fa3bb2125c22b438d7e653d0
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-28 21:58:25 +0000

    Split brew setup from updating custom scripts / plugins

---
+++ install_custom.sh
@@ -0,0 +1,43 @@
+# Install packages from github masters:
+
+# Needed for linux?
+if ! [ -e time-series-cop ]
+then
+git clone https://github.com/ctberthiaume/time-series-cop.git
+fi
+cd time-series-cop
+git pull
+npm install -g
+cd ..
+
+if ! [ -e data-integration-uploader ]
+then
+git clone https://github.com/ctberthiaume/data-integration-uploader.git
+fi
+cd data-integration-uploader
+git pull
+npm install
+zip -r ../data_uploader.zip *
+cd ..
+
+# Install grafana plugins
+
+if ! [ -e armbrustlab-2dscatter-panel ]
+then
+git clone https://github.com/ctberthiaume/armbrustlab-2dscatter-panel.git
+fi
+cd armbrustlab-2dscatter-panel
+git pull
+rm -rf /usr/local/var/lib/grafana/plugins/armbrustlab-2dscatter-panel
+cp -r dist /usr/local/var/lib/grafana/plugins/armbrustlab-2dscatter-panel
+cd ..
+
+if ! [ -e armbrustlab-cruisetrack-panel ]
+then
+git clone https://github.com/ctberthiaume/armbrustlab-cruisetrack-panel.git
+fi
+cd armbrustlab-cruisetrack-panel
+git pull
+rm -rf /usr/local/var/lib/grafana/plugins/armbrustlab-cruisetrack-panel
+cp -r dist /usr/local/var/lib/grafana/plugins/armbrustlab-cruisetrack-panel
+cd ..
