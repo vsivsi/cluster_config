10a1ef37738c4ce2a42c3487952a9f1c4398ab80
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-30 06:01:34 +0000

    Add backup and ingestions of TSV archives from ship

--- bin/ingest_ship_data.sh
+++ bin/ingest_ship_data.sh
@@ -10,3 +10,5 @@ echo "Ingesting ship data from: $1 for cruise: $2"
 lineprotocol-cnav_33103-GPVTG -c $2 -m cnav_speed_course -i <(cat $1/MGL-cnav.* | gawk 'NR%60==1 { print; getline; print; }') -H influx-head.service.consul -d data
 
 lineprotocol-cnav_33103-GPGGA -c $2 -m cnav_gps_position -i <(cat $1/MGL-cnav.* | gawk 'NR%60==1 { print; getline; print; }') -H influx-head.service.consul -d data
+
+lineprotocol-tsgraw_33721 -c $2 -m temp_sal_ss -i <(cat $1/MGL-tsgraw.* | gawk 'NR%3==1') -H influx-head.service.consul -d data
