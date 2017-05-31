fba256028f676efcaff86d06bc2192dd60354da0
Vaughn Iverson <vsivsi@yahoo.com>
2017-05-31 00:24:47 +0000

    Migrate the grafana and influxdb backups to restic 0.6.0

--- bin/influx_backup.sh
+++ bin/influx_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/usr/local/bin/restic init
+/usr/local/bin/restic6 init -o s3.layout=default
 /usr/local/bin/influxd backup -host influx-head.service.consul:8088 -database data /tmp/data
-/usr/local/bin/restic backup --hostname influx /tmp/data
+/usr/local/bin/restic6 backup --hostname influx /tmp/data
 /usr/local/bin/influxd backup -host influx-head.service.consul:8088 -database viz /tmp/viz
-/usr/local/bin/restic backup --hostname influx /tmp/viz
+/usr/local/bin/restic6 backup --hostname influx /tmp/viz
 rm -r /tmp/{data,viz}
