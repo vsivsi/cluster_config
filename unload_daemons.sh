e1f5e81d6612b3e9b61ad9ba76d0cd3026a93047
Vaughn Iverson <vsivsi@yahoo.com>
2017-10-06 16:27:18 -0700

    Disable copying peers.json as it didn't really work

--- unload_daemons.sh
+++ unload_daemons.sh
@@ -1,9 +1,9 @@
-# This will unload consul and nomad
+## This will unload consul and nomad
 
 sudo launchctl unload /Library/LaunchDaemons/com.hashicorp.nomad.plist
 sleep 3
 sudo launchctl unload /Library/LaunchDaemons/com.hashicorp.consul.plist
 
-# This will setup consul to reconnect on startup
-
-cp config/peers.json /usr/local/var/consul/raft
+## This will setup consul to reconnect on startup
+# Disables this for now because it didn't work anyway...
+# cp config/peers.json /usr/local/var/consul/raft
