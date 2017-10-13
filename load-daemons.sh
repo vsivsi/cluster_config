a368a5095727af35dfeaeb0bc0d105caabb386d3
Vaughn Iverson <vsivsi@yahoo.com>
2017-10-12 19:45:47 -0700

    Add caddy section to load-daemons script

--- load-daemons.sh
+++ load-daemons.sh
@@ -22,7 +22,22 @@ cp config/nomad_$NODETYPE.hcl /usr/local/etc/nomad.d
 
 sudo cp config/consul.plist /Library/LaunchDaemons/com.hashicorp.consul.plist
 sudo launchctl bootstrap system /Library/LaunchDaemons/com.hashicorp.consul.plist
+
 sudo cp config/nomad.plist /Library/LaunchDaemons/com.hashicorp.nomad.plist
 sudo launchctl bootstrap system /Library/LaunchDaemons/com.hashicorp.nomad.plist
+
 sudo mkdir -p /etc/resolver
 sudo cp config/consul.resolver /etc/resolver/consul
+
+if [[ $NODETYPE == "head" ]]
+then
+  if [[ $TF_VAR_EXTERNAL_HOSTNAME ]]
+  then
+    sudo launchctl setenv EXTERNAL_HOSTNAME ${TF_VAR_EXTERNAL_HOSTNAME}
+    sudo echo setenv EXTERNAL_HOSTNAME ${TF_VAR_EXTERNAL_HOSTNAME} > /etc/launchd.conf
+    sudo cp config/caddy.plist /Library/LaunchDaemons/com.mholt.caddy.plist
+    sudo launchctl bootstrap system /Library/LaunchDaemons/com.mholt.caddy.plist
+  else
+    echo "Caddy was not loaded because TF_VAR secrets are missing"
+  fi
+fi
